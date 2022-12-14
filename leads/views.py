57cc7fbd8830cd9791d2477e458b2dd72ef0cf94
Julio Mattos <juliocastellobranco@gmail.com>
2022-12-14 14:17:45 -0300

    Model Form Django

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 
 # Create your views here.
 
@@ -21,24 +21,39 @@ def lead_detail(request, pk):
     }
     return render(request,"leads/lead_detail.html", context)
 
+# Model Form Django
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            Lead.objects.create(
-                first_name = first_name,
-                last_name = last_name,
-                age = age,
-                agent = agent
-            )
+            form.save()
             return redirect("/leads")
     context = {
         "form": form
     }
     return render(request,"leads/lead_create.html", context)
+
+#Form Django
+# def lead_create(request):
+#     form = LeadForm()
+#     if request.method == "POST":
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             agent = Agent.objects.first()
+#             Lead.objects.create(
+#                 first_name = first_name,
+#                 last_name = last_name,
+#                 age = age,
+#                 agent = agent
+#             )
+#             return redirect("/leads")
+#     context = {
+#         "form": form
+#     }
+#     return render(request,"leads/lead_create.html", context)
+
